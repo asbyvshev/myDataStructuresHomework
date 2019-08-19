cc3d7c50c1ebef54ed15ab7ba078dff405afacb3
asbyvshev <barseto4ka@live.ru>
2019-08-19 13:22:06 +0300

    написан класс StackQueDequeTest для тестов Stack Queue Deque

--- src/test/java/StackQueDequeTest.java
+++ src/test/java/StackQueDequeTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.geekbrains.datastructure.lesson3.deque.DequeImpl;
+import ru.geekbrains.datastructure.lesson3.queue.Queue;
+import ru.geekbrains.datastructure.lesson3.queue.QueueImpl;
+import ru.geekbrains.datastructure.lesson3.stack.Stack;
+import ru.geekbrains.datastructure.lesson3.stack.StackImpl;
+
 public class StackQueDequeTest {
+    private Queue<Integer> queue;
+    private Queue<Integer> dequeue;
+    private Stack<Integer> stack;
+
+    @BeforeEach
+    public void prepare (){
+         queue = new QueueImpl<>(5);
+         dequeue = new DequeImpl<>(5);
+         stack = new StackImpl<>(5);
+    }
+
+    @Test
+    public void testRemoveStack() {}
+    @Test
+    public void testRemoveQueue() {}
+    @Test
+    public void testRemoveDequeRight() {}
+    @Test
+    public void testRemoveDequeLeft() {}
 }
