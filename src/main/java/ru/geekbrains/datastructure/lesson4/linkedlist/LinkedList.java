83ea8828a422a64e6fdcff211d32803e8a198024
asbyvshev <barseto4ka@live.ru>
2019-08-19 14:20:59 +0300

    перенесен код из 4го урока

---
+++ src/main/java/ru/geekbrains/datastructure/lesson4/linkedlist/LinkedList.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.datastructure.lesson4.linkedlist;
+
+import ru.geekbrains.datastructure.lesson2.ICollection;
+
+public interface LinkedList<E> extends ICollection/*, Iterable<E>*/ {
+
+    void insertFirst(E value);
+
+    E removeFirst();
+
+    boolean remove(E value);
+
+    boolean contains(E value);
+
+    void display();
+
+    E getFirstValue();
+
+
+    class Entry<T> {
+        public final T value;
+        public Entry<T> next;
+
+        public Entry(T value) {
+            this.value = value;
+        }
+    }
+
+}
