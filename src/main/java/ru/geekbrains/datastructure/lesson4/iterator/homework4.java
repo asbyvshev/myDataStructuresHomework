45894d7806821b4d709135d5354872ef4769de48
asbyvshev <barseto4ka@live.ru>
2019-08-22 16:17:02 +0300

    реализован forech в дз для листа.

--- src/main/java/ru/geekbrains/datastructure/lesson4/iterator/homework4.java
+++ src/main/java/ru/geekbrains/datastructure/lesson4/iterator/homework4.java
@@ -1,5 +1,17 @@
 package ru.geekbrains.datastructure.lesson4.iterator;
 
-public class homework4 {
+import ru.geekbrains.datastructure.lesson4.linkedlist.LinkedList;
+import ru.geekbrains.datastructure.lesson4.linkedlist.SimpleLinkedList;
 
+public class homework4 {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new SimpleLinkedList<>();
+        linkedList.insertFirst(1);
+        linkedList.insertFirst(2);
+        linkedList.insertFirst(3);
+
+        for (Integer value: linkedList) {
+            System.out.println(value);
+        }
+    }
 }
