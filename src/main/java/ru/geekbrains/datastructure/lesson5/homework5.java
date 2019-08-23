a68f4870a9da608fef6243d2a1afe6a77c44376e
asbyvshev <barseto4ka@live.ru>
2019-08-23 13:08:53 +0300

    1е задание ДЗ написано.

--- src/main/java/ru/geekbrains/datastructure/lesson5/homework5.java
+++ src/main/java/ru/geekbrains/datastructure/lesson5/homework5.java
@@ -1,4 +1,19 @@
 package ru.geekbrains.datastructure.lesson5;
 
 public class homework5 {
+
+    public static void main(String[] args) {
+        int nuber = 3;
+        int extent = 2;
+        System.out.println(myExponentiation(nuber,extent));
+    }
+
+    private static int myExponentiation(int nuber, int extent) {
+        if (nuber == 0){ return 0; }
+        if (extent < 1){ return 1; }
+        if (extent == 1){ return nuber; }
+        return nuber * myExponentiation(nuber,extent - 1);
+    }
+
+
 }
