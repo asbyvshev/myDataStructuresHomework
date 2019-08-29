5a714b3b7a9908f0cacd99335e339f0bb5f1764d
asbyvshev <barseto4ka@live.ru>
2019-08-29 12:22:45 +0300

    перенесено решение дз из 5го урока, перенес код 6 урока.

---
+++ src/main/java/ru/geekbrains/datastructure/lesson6/Tree.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.datastructure.lesson6;
+
+import ru.geekbrains.datastructure.lesson2.ICollection;
+
+public interface Tree<E> extends ICollection, Iterable<E> {
+
+    enum TraverseMode{
+        IN_ORDER,
+        PRE_ORDER,
+        POST_ORDER
+    }
+
+    boolean find(E value);
+
+    boolean add(E value);
+
+    boolean remove(E value);
+
+    void display();
+
+    boolean isBalanced();
+
+    void traverse(TraverseMode mode);
+}
