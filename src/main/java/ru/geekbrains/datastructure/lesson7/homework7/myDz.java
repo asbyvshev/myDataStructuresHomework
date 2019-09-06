bdfa04029df5d91abe1ec0b6fc18e8d480ee7686
asbyvshev <barseto4ka@live.ru>
2019-09-06 12:48:01 +0300

    создан пакет и класс для реализации дз

---
+++ src/main/java/ru/geekbrains/datastructure/lesson7/homework7/myDz.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.datastructure.lesson7.homework7;
+
+import ru.geekbrains.datastructure.lesson7.Graph;
+
+public class myDz {
+    public static void main(String[] args) {
+        Graph graph = new Graph(10);
+        graph.addVertex("Москва");
+        graph.addVertex("Тула");
+        graph.addVertex("Рязань");
+        graph.addVertex("Калуга");
+        graph.addVertex("Липецк");
+        graph.addVertex("Тамбов");
+        graph.addVertex("Орел");
+        graph.addVertex("Саратов");
+        graph.addVertex("Курск");
+        graph.addVertex("Воронеж");
+
+        graph.addEdges("Москва",
+                "Тула",
+                "Рязань",
+                "Калуга");
+
+        graph.addEdge("Тула", "Липецк");
+        graph.addEdge("Липецк", "Воронеж");
+
+        graph.addEdge("Рязань", "Тамбов");
+        graph.addEdge("Тамбов", "Саратов");
+        graph.addEdge("Саратов", "Воронеж");
+
+        graph.addEdge("Калуга", "Орел");
+        graph.addEdge("Орел", "Курск");
+        graph.addEdge("Курск", "Воронеж");
+
+
+    }
+}
